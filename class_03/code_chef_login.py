9bbfcda5bbf9df7b897fd0ddbd06fcd5c4cb501b
Igor Nunes Patricio <igornunespatricio@gmail.com>
2026-01-21 21:45:31 -0300

    feat: enhance CodeChef login script with CSRF token handling
    Updated the login process to fetch CSRF token and form_build_id from the login page using BeautifulSoup, ensuring secure authentication. Changed payload keys to match API requirements ("email" to "name", "password" to "pass") and switched to using a requests Session for persistent state. Added dashboard retrieval after successful login to verify functionality.

--- class_03/code_chef_login.py
+++ class_03/code_chef_login.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
 
 load_dotenv()
 headers = {
@@ -8,12 +9,31 @@ headers = {
 }
 EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
-payload = {"email": EMAIL, "password": PASSWORD, "form_id": "ajax_login_form"}
+payload = {"name": EMAIL, "pass": PASSWORD, "form_id": "ajax_login_form"}
+login_url = "https://www.codechef.com/api/codechef/login"
 
-response = requests.post(
-    "https://www.codechef.com/api/codechef/login", data=payload, headers=headers
-)
-if response.status_code == 200:
-    print("Login successful")
-else:
-    print("Login failed")
+with requests.Session() as s:
+    context = s.get(login_url, headers=headers)
+    soup = BeautifulSoup(context.content, "html.parser")
+    # needs to match the csrf token using lambda function because it has scape characters
+    csrf_token = (
+        soup.find("input", attrs={"name": lambda x: x and "csrfToken" in x})
+        .get("value")
+        .replace('\\"', "")
+    )
+    form_build_id = (
+        soup.find("input", attrs={"name": lambda x: x and "form_build_id" in x})
+        .get("value")
+        .replace('\\"', "")
+    )
+    payload["csrfToken"] = csrf_token
+    payload["form_build_id"] = form_build_id
+    print("oi")
+    response = s.post(login_url, data=payload, headers=headers)
+    if response.status_code == 200:
+        dashboard = s.get(
+            "https://www.codechef.com/api/learn/dashboard", headers=headers
+        )
+        print(dashboard.content)
+    else:
+        print("Login failed")
